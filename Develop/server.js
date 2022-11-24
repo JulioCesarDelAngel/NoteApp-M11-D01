4c59368cd6dc42615a13b980ffc2a8caa7712658
Julio C. MW11 <sysa.mail@gmail.com>
2022-11-23 19:20:14 -0600

    enrutamiento de pages

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
+const pagesRoutes = require('./routes/html.js');
 
 const PORT = 3001;
 const app = express();
 
 app.use(express.static('public'))
+
+app.use(pagesRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
