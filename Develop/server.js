934f0b10a998e32b90f4ba972feef632e2fa23bc
Julio C. MW11 <sysa.mail@gmail.com>
2022-11-24 16:12:29 -0600

    se agrega middleware y paquete  uuid

--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
-const pagesRoutes = require('./routes/html.js');
+const pagesRoutes = require('./routes/pages.js');
+const apiRoutes = require('./routes/api.js');
+
+const uuid = require('uuid');
+const fs = require('fs');
 
 const PORT = 3001;
 const app = express();
 
-app.use(express.static('public'))
+// Middleware para analizar los datos de JSON y de formularios codificados por url
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static('public'));
 
 app.use(pagesRoutes);
+app.use(apiRoutes);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
